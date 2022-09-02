db94abeaf64b4d3fe67405f7a5e75187f7c314df
Ishita Ghosh <ishitagh16@gmail.com>
2022-09-02 09:06:33 -0700

    Blog app is now independent

--- table/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from .models import Post
-
-class Post_Form(ModelForm):
-    class Meta:
-        model = Post
-        fields = ('num1', 'num2', 'Year', 'Month', 'File_Type', 'Consolidation_Type', 'Dealer_1', 'Dealer_2', 'Dealer_3', 'Dealer_4')
--- table/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-# from .views import PostCreateView, PostDetailView
-
-urlpatterns = [
-    path('', views.home, name='table-home'),
-    path('output/', views.output, name="table-output/"),
-    path('about/', views.about, name='table-about'),
-]
